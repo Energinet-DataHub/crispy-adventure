4500308b2350a55ab3942a1cbdcd65ff7cbcd6cd
Søren Søndergaard <s.sondergaard@icloud.com>
2023-01-30 15:26:03 +0100

    set connectionstring

--- src/Cave/Program.cs
+++ src/Cave/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+const string ConnectionString =
+    "Server=tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID=mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;";
+
+
 Console.WriteLine("Hello, World!");
